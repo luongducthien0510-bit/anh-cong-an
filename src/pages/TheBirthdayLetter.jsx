ee26840fd0b495a565c02aa8a64311ad2e88deb1
ThienLuong <luongducthien0510@gmail.com>
2026-05-31 22:51:34 +0700

    Sửa thẻ p đóng trong TheBirthdayLetter

--- src/pages/TheBirthdayLetter.jsx
+++ src/pages/TheBirthdayLetter.jsx
@@ -19,6 +19,7 @@ function TheBirthdayLetter() {
                 <div className="letter">
                     <p>
                    Em có đôi điều muốn gửi đến anh. Em đã rất đắn đo suy nghĩ khá nhiều từ hôm qua. Trước tiên, em muốn trải lòng là em rất có thiện cảm với anh, cảm ơn anh thời gian qua đã quan tâm đến em. Thời gian tuy ngắn nhưng thực sự anh mang đến cho em cảm giác rất thoải mái và vui vẻ mỗi khi trò chuyện, thật sự rất khó và rất lâu rồi em mới làm quen được một người khác giới mà bắt sóng nhanh với em và vui tính như vậy. Em thực sự cảm thấy rất quý anh nhưng mà vì vài lí do nên em nghĩ là mình nên dừng lại ở đây (lí do cụ thể thì mẹ e đã nói với cô để trao đổi lại với mẹ anh). Em cảm ơn anh ạ. 
+                   </p>
                     <p style={{ textAlign: "right" }}>Người gửi</p>
                     <p style={{ textAlign: "right" }}>Cô gái nhỏ bé yêu thiên nhiên</p>
                 </div>
